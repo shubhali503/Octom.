df7df30e91ea67f0d458958a0666d0391a4ea932
shubhali503 <shubhaliagarwal555@gmail.com>
2022-09-07 12:03:00 +0530

    modified

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,18 @@ fetch(`https://mitopeci.sirv.com/sprite.svg`).then(res => {
 }).then(data => {
   document.getElementById('svg-icons').innerHTML = data;
 });
+
+const headerProfileUser = document.querySelector(".header__profile-user");
+
+const userName = document.querySelector(".user-name");
+const tooltipUserName = document.querySelector(".tooltip-username");
+const userEmail = document.querySelector(".user-email");
+const tooltipEmail = document.querySelector(".tooltip-email");
+
+headerProfileUser.addEventListener("click", () => headerProfileUser.classList.toggle("active"));
+
+let userNameText = document.createTextNode(userName.textContent);
+tooltipUserName.appendChild(userNameText);
+
+let userEmailText = document.createTextNode(userEmail.textContent);
+tooltipEmail.appendChild(userEmailText);
